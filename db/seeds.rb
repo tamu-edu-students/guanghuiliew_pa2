132c6de558ff3f7fc619d47f8176489ee2c03fb5
ghliew <liewgh1995@gmail.com>
2023-09-26 21:32:49 -0500

    updated new movie

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,9 @@ more_movies = [
   {:title => 'Nomadland', :rating => 'R',
     :release_date => '19-Feb-2021'},
   {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'}
+    :release_date => '13-Aug-2021'},
+  {:title => 'Your Name', :rating => 'PG',
+    :release_date => '7-Apr-2017'}
 ]
 
 more_movies.each do |movie|
